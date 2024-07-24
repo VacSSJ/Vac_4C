c6963a511617a851744cecbb9b6a0b417d93ea0a
VacSSJ <vicentearaya@liceovvh.cl>
2024-07-24 10:22:23 -0400

    ok

--- P.O.O-main/Fundamentos JS/05_Bucles/Ejercicios/04_EjerciciosFor/script.js
+++ P.O.O-main/Fundamentos JS/05_Bucles/Ejercicios/04_EjerciciosFor/script.js
@@ -5,12 +5,10 @@ function Verificar(){
     for(let i = 0; i < 5; i++){
         let numero = parseInt(prompt("Ingrese el numero " + i + ":"))
         array.push(numero)
-        /*
         if (numeroMayor < numero){
             numeroMayor = numero }else{
                 numeroMayor = numeroMayor
-        }*/
-        numeroMayor = (numeroMayor < numero) ? num : numeroMayor
+        }
     }
 document.getElementById(`lista`).innerHTML = `<p>${array.join(", ")} el numero mayor es: <h1>${numeroMayor}</h1></p>`
 }
